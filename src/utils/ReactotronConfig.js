4fc7d6187d6432aa7c146589b77563780c9bc17c
Welington da Silva <thebylito@gmail.com>
2018-11-23 23:34:15 -0300

    configure Reactotron

--- src/utils/ReactotronConfig.js
+++ src/utils/ReactotronConfig.js
@@ -4,9 +4,9 @@ import sagaPlugin from 'reactotron-redux-saga';
 
 if (__DEV__) {
   const tron = Reactotron.configure()
-    .useReactNative()
     .use(reactotronRedux())
     .use(sagaPlugin())
+    .useReactNative()
     .connect();
 
   console.tron = tron;
